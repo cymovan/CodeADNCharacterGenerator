69493452f08dd0fd1b9f43d2fe39a7d501b0fb65
cdelr <cdelr@192.168.1.50>
2023-05-05 18:49:51 +0200

    Fix : Rules not respected for enemy history list 7 values and no more + JUnit.

--- CodeADNCharacterGenerator/src/main/java/moon/adn/code/model/character/history/event/HistoryEnemyEvent.java
+++ CodeADNCharacterGenerator/src/main/java/moon/adn/code/model/character/history/event/HistoryEnemyEvent.java
@@ -3,8 +3,7 @@ package moon.adn.code.model.character.history.event;
 import moon.adn.code.model.character.history.HistoryEventAbstract;
 
 public class HistoryEnemyEvent extends HistoryEventAbstract {
-	private static final int[] INTERVAL_VALUES = { 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72,
-			76, 80, 84, 88, 92, 96, 100 };
+	private static final int[] INTERVAL_VALUES = { 10, 20, 30, 40, 50, 60, 100 };
 	protected static final int MAX_INTERVAL = 100;
 	private static final String ENEMY_PREFIX = "enemy";
 
---
+++ CodeADNCharacterGenerator/src/test/java/moon/adn/code/model/character/history/RAEC/event/EnemyEventTests.java
@@ -0,0 +1,26 @@
+package moon.adn.code.model.character.history.RAEC.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import moon.adn.code.model.character.history.event.HistoryEnemyEvent;
+
+@SpringBootTest
+public class EnemyEventTests {
+
+	private static final int maxCountI18N = 6;
+	private static final String I18NRoot = "hist.enemy"; 
+
+	@Test
+	public void whenInitializingRandomEnemyEventThenItsInRangeDefinedInRulesBook() {
+		HistoryEnemyEvent hee = new HistoryEnemyEvent();
+		for (int i = 0; i < 100; i++) {
+			String result = hee.randomEvent();
+			int value = Integer.parseInt(result.substring(10));
+			assertThat(value).isBetween(0, maxCountI18N);
+		}
+	}
+
+}
